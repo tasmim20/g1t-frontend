373e4deed6a7931738e17582ee24d1ec010c1ba2
tasmim20 <tasmimrahman2000@gmail.com>
2025-11-12 15:05:45 +0600

    fix

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,12 +1,3 @@
-// import { createApi } from "@reduxjs/toolkit/query/react";
-// import { axiosBaseQuery } from "../../helpers/axios/axiosBaseQuery";
-
-// export const baseApi = createApi({
-//   reducerPath: "baseApi",
-//   baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000" }),
-//   endpoints: () => ({}),
-// });
-
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { axiosBaseQuery } from "../../helpers/axios/axiosBaseQuery";
 
